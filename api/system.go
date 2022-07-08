d47f8c379c80270e842fb30a210d35286b6d5803
silver-ts <silvertechsavvy@gmail.com>
2022-07-08 22:16:18 +0800

    chore: rename user role (#108)
    * chore: rename user role to `host`
    
    * chore: related frontend changes
    
    * chore: fix migration file
    
    * chore: use tricky sql

--- api/system.go
+++ api/system.go
@@ -3,6 +3,6 @@ package api
 import "github.com/silver-ts/memos/server/profile"
 
 type SystemStatus struct {
-	Owner   *User            `json:"owner"`
+	Host    *User            `json:"host"`
 	Profile *profile.Profile `json:"profile"`
 }
